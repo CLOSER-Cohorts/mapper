427962d7213e69162e3aace9ef082423165382bb
William Poynter <poynter.william@gmail.com>
2015-12-02 07:41:47 +0000

    Edited tests to be ready for new fixtures

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class QuestionsControllerTest < ActionController::TestCase
   setup do
-    @question = questions(:one)
+    @question = Question.all.first
   end
 
   test "should get index" do
